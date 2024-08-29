5da8ad740d6c78d5b3d4447d37b139532cf8a64a
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-28 18:19:51 -0600

    Update UIManager.js

--- UIManager.js
+++ UIManager.js
@@ -81,7 +81,7 @@ export class UIManager {
                     this.showTowerUpgradeMenu(clickedTower);
                 }
             });
-        }
+        )}
     }
 
     createMenuButtons() {
